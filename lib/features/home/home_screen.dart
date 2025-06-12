293af2ab32c35eade626ff0a3a13548701e48a0f
Alexander <muthuaalex@gmail.com>
2025-06-12 15:05:27 +0300

    COnverting StatelessWidget to StatefulWidget
    I have done this to Incorporate the refresh status on the application

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen>  {
 
   void _logout(BuildContext context) async {
     await Supabase.instance.client.auth.signOut();

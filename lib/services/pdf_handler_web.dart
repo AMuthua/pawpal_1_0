56ec7e8d48aaa701610af85ed784b23e88e479a4
Alexander <muthuaalex@gmail.com>
2025-07-08 14:35:45 +0300

    Update pdf_handler_web.dart

--- lib/services/pdf_handler_web.dart
+++ lib/services/pdf_handler_web.dart
@@ -1,30 +1,39 @@
 
-
 // lib/services/pdf_handler_web.dart
 import 'dart:typed_data';
-import 'dart:html' as html; // Allowed ONLY in web
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
 
 import 'pdf_handler_base.dart';
 
 class PdfHandlerWeb extends PdfHandlerBase {
   @override
   Future<void> generateAndHandleReceipt(Map<String, dynamic> booking) async {
-    final Uint8List pdfBytes = await generatePdfBytes(booking);
-    
+    final pdfBytes = await PdfHandlerBase.generatePdfBytes(booking);
+    final filename = 'pawpal_receipt_${DateTime.now().millisecondsSinceEpoch}.pdf';
+
     final blob = html.Blob([pdfBytes], 'application/pdf');
     final url = html.Url.createObjectUrlFromBlob(blob);
     final anchor = html.AnchorElement(href: url)
-      ..download = 'pawpal_receipt.pdf'
+      ..setAttribute('download', filename)
       ..click();
-
     html.Url.revokeObjectUrl(url);
   }
 }
+// No createPdfHandler() needed here anymore, as it's directly instantiated in pdf_receipt_service.dart
 
 
-// // Try and comment it to see
-// //  whether it will compile without this. for mobile app. 
-// IT HAS!!!!
-// SO TO MAKE MOBILE i NEED TO COMMENT THIS PAGE OUT AND PDF RECEIPT SERVICE. 
-// The Good thing is that, It has no problem to compile the app as well for web usage. 
-        // Too early to say?
+
+
+
+
+
+
+
+
+// // // Try and comment it to see
+// // //  whether it will compile without this. for mobile app. 
+// // IT HAS!!!!
+// // SO TO MAKE MOBILE i NEED TO COMMENT THIS PAGE OUT AND PDF RECEIPT SERVICE. 
+// // The Good thing is that, It has no problem to compile the app as well for web usage. 
+//         // Too early to say?

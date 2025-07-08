d56db1375e4af6fcc966c913a924800d421894b7
Alexander <muthuaalex@gmail.com>
2025-07-08 15:22:32 +0300

    PDF handler logic
    restructuring the logic to make the pdf look and fetch all the information better. from all ends.

--- lib/services/pdf_handler_mobile.dart
+++ lib/services/pdf_handler_mobile.dart
@@ -1,5 +1,33 @@
 
 
+// // lib/services/pdf_handler_mobile.dart
+// import 'dart:typed_data';
+// import 'dart:io' as io;
+
+// import 'package:path_provider/path_provider.dart';
+// import 'package:share_plus/share_plus.dart';
+
+// import 'pdf_handler_base.dart';
+
+// class PdfHandlerMobile extends PdfHandlerBase {
+//   @override
+//   Future<void> generateAndHandleReceipt(Map<String, dynamic> booking) async {
+//     final pdfBytes = await PdfHandlerBase.generatePdfBytes(booking);
+//     final filename = 'pawpal_receipt_${DateTime.now().millisecondsSinceEpoch}.pdf';
+
+//     final dir = await getTemporaryDirectory();
+//     final filePath = '${dir.path}/$filename';
+//     final file = io.File(filePath);
+//     await file.writeAsBytes(pdfBytes);
+
+//     await Share.shareXFiles([XFile(file.path)], text: 'Here is your PawPal receipt 🧾');
+//   }
+// }
+// // No createPdfHandler() needed here anymore, as it's directly instantiated in pdf_receipt_service.dart
+
+
+
+
 // lib/services/pdf_handler_mobile.dart
 import 'dart:typed_data';
 import 'dart:io' as io;
@@ -12,7 +40,7 @@ import 'pdf_handler_base.dart';
 class PdfHandlerMobile extends PdfHandlerBase {
   @override
   Future<void> generateAndHandleReceipt(Map<String, dynamic> booking) async {
-    final pdfBytes = await PdfHandlerBase.generatePdfBytes(booking);
+    final pdfBytes = await PdfHandlerBase.generatePdfBytes(booking); // Corrected call
     final filename = 'pawpal_receipt_${DateTime.now().millisecondsSinceEpoch}.pdf';
 
     final dir = await getTemporaryDirectory();
@@ -23,4 +51,3 @@ class PdfHandlerMobile extends PdfHandlerBase {
     await Share.shareXFiles([XFile(file.path)], text: 'Here is your PawPal receipt 🧾');
   }
 }
-// No createPdfHandler() needed here anymore, as it's directly instantiated in pdf_receipt_service.dart
--- lib/services/pdf_handler_web.dart
+++ lib/services/pdf_handler_web.dart
@@ -1,4 +1,29 @@
 
+// // lib/services/pdf_handler_web.dart
+// import 'dart:typed_data';
+// // ignore: avoid_web_libraries_in_flutter
+// import 'dart:html' as html;
+
+// import 'pdf_handler_base.dart';
+
+// class PdfHandlerWeb extends PdfHandlerBase {
+//   @override
+//   Future<void> generateAndHandleReceipt(Map<String, dynamic> booking) async {
+//     final pdfBytes = await PdfHandlerBase.generatePdfBytes(booking);
+//     final filename = 'pawpal_receipt_${DateTime.now().millisecondsSinceEpoch}.pdf';
+
+//     final blob = html.Blob([pdfBytes], 'application/pdf');
+//     final url = html.Url.createObjectUrlFromBlob(blob);
+//     final anchor = html.AnchorElement(href: url)
+//       ..setAttribute('download', filename)
+//       ..click();
+//     html.Url.revokeObjectUrl(url);
+//   }
+// }
+// // No createPdfHandler() needed here anymore, as it's directly instantiated in pdf_receipt_service.dart
+
+
+
 // lib/services/pdf_handler_web.dart
 import 'dart:typed_data';
 // ignore: avoid_web_libraries_in_flutter
@@ -9,7 +34,7 @@ import 'pdf_handler_base.dart';
 class PdfHandlerWeb extends PdfHandlerBase {
   @override
   Future<void> generateAndHandleReceipt(Map<String, dynamic> booking) async {
-    final pdfBytes = await PdfHandlerBase.generatePdfBytes(booking);
+    final pdfBytes = await PdfHandlerBase.generatePdfBytes(booking); // Corrected call
     final filename = 'pawpal_receipt_${DateTime.now().millisecondsSinceEpoch}.pdf';
 
     final blob = html.Blob([pdfBytes], 'application/pdf');
@@ -20,20 +45,15 @@ class PdfHandlerWeb extends PdfHandlerBase {
     html.Url.revokeObjectUrl(url);
   }
 }
-// No createPdfHandler() needed here anymore, as it's directly instantiated in pdf_receipt_service.dart
 
 
 
 
 
 
-
-
-
-
-// // // Try and comment it to see
-// // //  whether it will compile without this. for mobile app. 
-// // IT HAS!!!!
-// // SO TO MAKE MOBILE i NEED TO COMMENT THIS PAGE OUT AND PDF RECEIPT SERVICE. 
-// // The Good thing is that, It has no problem to compile the app as well for web usage. 
-//         // Too early to say?
+// // // // Try and comment it to see
+// // // //  whether it will compile without this. for mobile app. 
+// // // IT HAS!!!!
+// // // SO TO MAKE MOBILE i NEED TO COMMENT THIS PAGE OUT AND PDF RECEIPT SERVICE. 
+// // // The Good thing is that, It has no problem to compile the app as well for web usage. 
+// //         // Too early to say?

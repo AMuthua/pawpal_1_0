a9bd77085db57f12f8985055415b2b4f4662efd0
Alexander <muthuaalex@gmail.com>
2025-07-08 14:35:43 +0300

    Update pdf_handler_mobile.dart

--- lib/services/pdf_handler_mobile.dart
+++ lib/services/pdf_handler_mobile.dart
@@ -1,23 +1,26 @@
+
+
 // lib/services/pdf_handler_mobile.dart
-import 'dart:io';
 import 'dart:typed_data';
+import 'dart:io' as io;
+
 import 'package:path_provider/path_provider.dart';
 import 'package:share_plus/share_plus.dart';
-import 'package:pdf/widgets.dart' as pw;
 
 import 'pdf_handler_base.dart';
 
 class PdfHandlerMobile extends PdfHandlerBase {
   @override
   Future<void> generateAndHandleReceipt(Map<String, dynamic> booking) async {
-    final Uint8List pdfBytes = await generatePdfBytes(booking);
+    final pdfBytes = await PdfHandlerBase.generatePdfBytes(booking);
+    final filename = 'pawpal_receipt_${DateTime.now().millisecondsSinceEpoch}.pdf';
 
     final dir = await getTemporaryDirectory();
-    final filePath = '${dir.path}/pawpal_receipt.pdf';
-
-    final file = File(filePath);
+    final filePath = '${dir.path}/$filename';
+    final file = io.File(filePath);
     await file.writeAsBytes(pdfBytes);
 
-    await Share.shareXFiles([XFile(file.path)], text: '🧾 Here is your receipt!');
+    await Share.shareXFiles([XFile(file.path)], text: 'Here is your PawPal receipt 🧾');
   }
 }
+// No createPdfHandler() needed here anymore, as it's directly instantiated in pdf_receipt_service.dart
